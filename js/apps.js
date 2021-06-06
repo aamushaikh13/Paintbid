57d3f221f492554eefde0a93fc7150642c1b54ee
aamushaikh13 <aamushaikh13@gmail.com>
2021-06-06 11:56:34 +0530

    changes commited

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,11 @@
-function showMessage() {
-    alert('Cliked on button');
+//declare the function : myName
+function myName(){
+    var name = prompt("what is your name");
+    return name;
 }
+//myName function calling
+var name =myName();
+console.log(name);
+document.write("<br/>Thanks for Biding  ",name);
+
+
